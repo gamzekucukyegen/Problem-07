9dbbbd7fe070fd52c09d11a49c0ebd995c7a763c
gamzekucukyegen <you@example.com>
2024-09-19 21:14:54 +0300

    problem-06

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+
+function Avatar({profileText,imgSrc,name}) {
+  return (
+    <div className='group block p-8 flex-shrink-0'>
+      <div className='flex items-center'>
+        <div>
+          <Image
+            className='inline-block h-12 w-12 rounded-full'
+            src={imgSrc}
+            alt=''
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className='ml-3'>
+          <p className='text-base font-medium text-gray-700 group-hover:text-gray-900'>
+            {name}
+          </p>
+          <p className='text-sm font-medium text-gray-500 group-hover:text-gray-700'>
+            {profileText}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function App() {
+  return <Avatar profileText='Profile bakın' imgSrc='/photo.png' name="Tom Cook"/>
+}
